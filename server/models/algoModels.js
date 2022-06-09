b0129aae3cc2bfe8fc466402a5de2bd5fa7abfc7
Kai Zhang <kaizen.agility@gmail.com>
2022-06-09 09:52:00 -0400

    Update algoModels.js

--- server/models/algoModels.js
+++ server/models/algoModels.js
@@ -3,11 +3,14 @@ const Schema = mongoose.Schema;
 
 // schema for 'algo'
 const algoSchema = new Schema({
-    id: Number,
+    questionID: String,
     name: String,
     link: String,
     level: String,
-    pattern: String
+    pattern: String,
+    description: String,
+    companies: [String],
+    resources: [String]
 });
 
 const algoModel = mongoose.model('questions', algoSchema );
